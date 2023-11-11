aaaa7ccfea23802be6f5681d9cf267f1967885fb
Vinicio Eufrazio <vinicio.eufrazio@aluno.ifsertao-pe.edu.br>
2023-11-11 13:42:10 -0300

    api rodando lisin

--- app.js
+++ app.js
@@ -1,12 +1,79 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require("./models/Artigo");
+
+const Artigo = mongoose.model('artigo');
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) =>{
-    return res.json({titulo: "ESSA PESTE TA DANDO ERROO"});
+mongoose.connect('mongodb://127.0.0.1/labmaker').then(() => {
+    console.log("Conexão realizada com sucesso!");
+}).catch((erro) =>{
+    console.log("Ocorreu um erro na conexão!");
 });
+
+app.get("/", async (req, res) =>{
+    Artigo.find({}).then((artigo) =>{
+        return res.json(artigo);
+    }).catch((err) =>{
+        return res.status(400).json({
+            error: true,
+            message: "Nenhum artigo encontrado"
+        });
+    });
+});
+
+app.get("/artigo/:id", (req, res) => {
+    Artigo.findOne({ _id: req.params.id }).then((artigo) => {
+        return res.json(artigo);
+    }).catch((erro) => {
+        return res.status(400).json({
+            error: true,
+            message: "Nenhum artigo encontrado!"
+        });
+    });
+});
+
+app.post("/artigo", async (req, res) => {
+    try {
+        const artigo = await Artigo.create(req.body);
+        return res.status(200);
+    } catch (err) {
+        return res.status(400);
+    }
+});
+
+app.get("/artigo/:id", (req, res) => {
+    Artigo.findOne({ _id: req.params.id }).then((artigo) => {
+        return res.json(artigo);
+    }).catch((erro) => {
+        return res.status(400).json({
+            error: true,
+            message: "Nenhum artigo encontrado!"
+        });
+    });
+});
+
+app.put("/artigo/editar/:id", (req, res) => {
+    const artigo = Artigo.updateOne({_id: req.params.id}, req.body)
+        .exec()
+        .then(() => {
+            return res.status(200).json({
+                erro: false,
+                message: "Alterações feitas"
+            });
+        })
+        .catch((erro) => {
+            return res.status(400).json({
+                erro: true,
+                message: "Ocorreu um problema, tente novamente"
+            });
+        });
+});
+
 
 app.listen(8080, ()=>{
     console.log("Servidor iniciado na porta 8080: http://localhost:8080");
---
+++ models/Artigo.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Artigo = new mongoose.Schema({
+    titulo:{
+        type: String,
+        require: true
+    },
+    conteudo:{
+        type: String,
+        require: true
+    }
+},
+{
+    timestamps: true,
+});
+
+mongoose.model('artigo', Artigo);
