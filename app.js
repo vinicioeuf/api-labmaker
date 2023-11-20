35f26091a1c8e41aa77f0cea82d3c869e5b8c178
Vinicio Eufrazio <59233562+vinicioeuf@users.noreply.github.com>
2023-11-20 13:01:28 -0300

    .

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const port = process.env.PORT || 3000;
 
 require("./models/Usuarios");
 require("./models/Acessos");
-
+require("./models/Acoes");
 // const Artigo = mongoose.model('artigo');
 const Usuarios = mongoose.model('usuarios');
+const Acoes = mongoose.model('acoes');
 const Acessos = mongoose.model('acessos');
 
 const app = express();
