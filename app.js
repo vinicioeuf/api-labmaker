00e11b845449445374d6fd25ffe43651be2ee35a
Vinicio Eufrazio <59233562+vinicioeuf@users.noreply.github.com>
2024-04-12 16:44:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb://mongo:QhtdBUKuMlfBsdVdykLpRdWZpjUBkaiB@roundhouse.proxy.rlwy.net:40026').then(() => {
+mongoose.connect('mongodb://mongo:czdHMXzMRQkSDQCgDGetXXVDaaUaAyWY@roundhouse.proxy.rlwy.net:32428').then(() => {
     console.log("Conexão realizada com sucesso!");
 }).catch((erro) =>{
     console.log("Ocorreu um erro na conexão!");
