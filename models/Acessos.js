4091b5768ab40288f77dd54daafe03dc7eff29c1
Vinicio Eufrazio <59233562+vinicioeuf@users.noreply.github.com>
2024-06-27 13:03:35 -0300

    Revert "update Acessos.js"
    This reverts commit e46562e6028ec42d0946b5853ddaa7e0558bae48.

--- models/Acessos.js
+++ models/Acessos.js
@@ -7,8 +7,8 @@ const getAcessos = async () => {
 };
 
 const createAcesso = async (data) => {
-    const { usuario_id, createdAt } = data;
-    const res = await pool.query('INSERT INTO acessos (usuario_id, createdAt) VALUES ($1, $2) RETURNING *', [usuario_id, createdAt]);
+    const { usuario_id, data_acesso } = data;
+    const res = await pool.query('INSERT INTO acessos (usuario_id, data_acesso) VALUES ($1, $2) RETURNING *', [usuario_id, data_acesso]);
     return res.rows[0];
 };
 
