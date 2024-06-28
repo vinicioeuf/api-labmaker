125f562ec46634f13dbd46df4c7bc0392b7daee5
VictorC78 <victorcarlos0000000000@gmail.com>
2024-06-27 22:14:09 -0300

    Reapply "ok"
    This reverts commit 3128a1686cc1debbaad7ed857c186fd4dec4b22a.

--- app.js
+++ app.js
@@ -1,51 +1,41 @@
-
+// app.js
 process.env.TZ = 'America/Sao_Paulo';
 const express = require('express');
-const mongoose = require('mongoose');
-const port = process.env.PORT || 3000;
-require("./models/Usuarios");
-require("./models/Acessos");
-// require("./models/Acoes");
-// const Artigo = mongoose.model('artigo');
-const Usuarios = mongoose.model('usuarios');
-// const Acoes = mongoose.model('acoes');
-const Acessos = mongoose.model('acessos');
+const { getUsuarios, getUsuarioById, createUsuario, updateUsuario } = require('./models/Usuarios');
+const { getAcessos, createAcesso } = require('./models/Acessos');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb://mongo:ThJcSWUqpiyQnmjuiOBeuSVmBYodyXPi@viaduct.proxy.rlwy.net:25158').then(() => {
-    console.log("Conexão realizada com sucesso!");
-}).catch((erro) =>{
-    console.log("Ocorreu um erro na conexão!");
-});
-
-app.get("/", async (req, res) =>{
-    Usuarios.find({}).then((usuarios) =>{
-        return res.json({usuarios});
-    }).catch((err) =>{
+app.get("/", async (req, res) => {
+    try {
+        const usuarios = await getUsuarios();
+        return res.json({ usuarios });
+    } catch (err) {
         return res.status(400).json({
             error: true,
             message: "Ocorreu um erro na API, estamos resolvendo!"
         });
-    });
+    }
 });
 
-app.get("/acessos", async (req, res) =>{
-    Acessos.find({}).then((acessos) =>{
-        return res.json({acessos});
-    }).catch((err) =>{
+app.get("/acessos", async (req, res) => {
+    try {
+        const acessos = await getAcessos();
+        return res.json({ acessos });
+    } catch (err) {
         return res.status(400).json({
             error: true,
             message: "Nenhum acesso até o momento!"
         });
-    });
+    }
 });
 
 app.post("/addusuarios", async (req, res) => {
     try {
-        const usuarios = await Usuarios.create(req.body);
+        const usuario = await createUsuario(req.body);
         return res.status(200).send("Usuário adicionado com sucesso");
     } catch (err) {
         return res.status(400).send("Erro ao adicionar usuário");
@@ -54,42 +44,40 @@ app.post("/addusuarios", async (req, res) => {
 
 app.post("/addacessos", async (req, res) => {
     try {
-        const acessos = await Acessos.create(req.body);
+        const acesso = await createAcesso(req.body);
         return res.status(200).send("Acesso adicionado com sucesso");
     } catch (err) {
-        return res.status(400).send("Erro ao adicionar acesso", res);
+        return res.status(400).send("Erro ao adicionar acesso");
     }
 });
 
-
-app.get("/listarusuario/:id", (req, res) => {
-    Usuarios.findOne({ _id: req.params.id }).then((usuario) => {
+app.get("/listarusuario/:id", async (req, res) => {
+    try {
+        const usuario = await getUsuarioById(req.params.id);
         return res.json(usuario);
-    }).catch((erro) => {
+    } catch (err) {
         return res.status(400).json({
             error: true,
             message: "Nenhum usuario com o id que você inseriu foi encontrado!"
         });
-    });
+    }
 });
 
-app.put("/usuarios/editar/:id", (req, res) => {
-    const usuario = Usuarios.updateOne({_id: req.params.id}, req.body)
-        .exec()
-        .then(() => {
-            return res.status(200).json({
-                erro: false,
-                message: "Alterações feitas"
-            });
-        })
-        .catch((erro) => {
-            return res.status(400).json({
-                erro: true,
-                message: "Ocorreu um problema, tente novamente"
-            });
+app.put("/usuarios/editar/:id", async (req, res) => {
+    try {
+        const usuario = await updateUsuario(req.params.id, req.body);
+        return res.status(200).json({
+            erro: false,
+            message: "Alterações feitas"
         });
+    } catch (err) {
+        return res.status(400).json({
+            erro: true,
+            message: "Ocorreu um problema, tente novamente"
+        });
+    }
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Servidor iniciado no endereço: http://localhost:${port}`);
 });
--- models/Usuarios.js
+++ models/Usuarios.js
@@ -1,42 +1,26 @@
-const { ObjectId } = require('mongodb');
-const mongoose = require('mongoose');
+// models/Usuarios.js
+const pool = require('../e');
 
-const Usuarios = new mongoose.Schema({
-    // _id:{
-    //     type: ObjectId,
-    //     required: true
-    // },
-    nome: {
-        type: String,
-        required: false
-    },
-    email: {
-        type: String,
-        required: false
-    },
-    idBiometria: {
-        type: Number,
-        required: false
-    },
-    foto: {
-        type: String,
-        required: false
-    },
-    status:{
-        type: String,
-        required: false
-    }
+const getUsuarios = async () => {
+    const res = await pool.query('SELECT * FROM usuarios');
+    return res.rows;
+};
 
-},
-{
-    timestamps: true,
-});
+const getUsuarioById = async (id) => {
+    const res = await pool.query('SELECT * FROM usuarios WHERE id = $1', [id]);
+    return res.rows[0];
+};
 
-// Usuarios.pre('save', function(next) {
-//     if (!this.idBiometria) {
-//         this.idBiometria = Math.floor(Math.random() * 120) + 1;
-//     }
-//     next();
-// });
+const createUsuario = async (data) => {
+    const { nome, email } = data;
+    const res = await pool.query('INSERT INTO usuarios (nome, email) VALUES ($1, $2) RETURNING *', [nome, email]);
+    return res.rows[0];
+};
 
-mongoose.model('usuarios', Usuarios);
+const updateUsuario = async (id, data) => {
+    const { nome, email } = data;
+    const res = await pool.query('UPDATE usuarios SET nome = $1, email = $2 WHERE id = $3 RETURNING *', [nome, email, id]);
+    return res.rows[0];
+};
+
+module.exports = { getUsuarios, getUsuarioById, createUsuario, updateUsuario };
