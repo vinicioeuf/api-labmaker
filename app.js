f3e1eff604395fa9a4609dcf0b13d655e89d1db1
Vinicio Eufrazio <59233562+vinicioeuf@users.noreply.github.com>
2024-06-27 23:08:27 -0300

    Revert "pg"
    This reverts commit 27a6c2e27ed6947209ee1d242e9c69a8e500ede4.

--- app.js
+++ app.js
@@ -1,90 +1,95 @@
-// app.js
+
 process.env.TZ = 'America/Sao_Paulo';
 const express = require('express');
-const { getUsuarios, getUsuarioById, createUsuario, updateUsuario } = require('./models/Usuarios');
-const { getAcessos, createAcesso } = require('./models/Acessos');
-
+const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
+require("./models/Usuarios");
+require("./models/Acessos");
+// require("./models/Acoes");
+// const Artigo = mongoose.model('artigo');
+const Usuarios = mongoose.model('usuarios');
+// const Acoes = mongoose.model('acoes');
+const Acessos = mongoose.model('acessos');
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-    try {
-        const usuarios = await getUsuarios();
-        return res.json({ usuarios });
-    } catch (err) {
-        console.error(err);
+mongoose.connect('mongodb://mongo:EkPkpiTjaeWUuRaRaucOgrofQQSEMfAr@monorail.proxy.rlwy.net:32765').then(() => {
+    console.log("Conexão realizada com sucesso!");
+}).catch((erro) =>{
+    console.log("Ocorreu um erro na conexão!");
+});
+
+app.get("/", async (req, res) =>{
+    Usuarios.find({}).then((usuarios) =>{
+        return res.json({usuarios});
+    }).catch((err) =>{
         return res.status(400).json({
             error: true,
             message: "Ocorreu um erro na API, estamos resolvendo!"
         });
-    }
+    });
 });
 
-app.get("/acessos", async (req, res) => {
-    try {
-        const acessos = await getAcessos();
-        return res.json({ acessos });
-    } catch (err) {
-        console.error(err);
+app.get("/acessos", async (req, res) =>{
+    Acessos.find({}).then((acessos) =>{
+        return res.json({acessos});
+    }).catch((err) =>{
         return res.status(400).json({
             error: true,
             message: "Nenhum acesso até o momento!"
         });
-    }
+    });
 });
 
 app.post("/addusuarios", async (req, res) => {
     try {
-        const usuario = await createUsuario(req.body);
+        const usuarios = await Usuarios.create(req.body);
         return res.status(200).send("Usuário adicionado com sucesso");
     } catch (err) {
-        console.error("Erro ao adicionar usuário:", err);
-        return res.status(400).json({ error: "Erro ao adicionar usuário", details: err.message });
+        return res.status(400).send("Erro ao adicionar usuário");
     }
 });
 
 app.post("/addacessos", async (req, res) => {
     try {
-        const acesso = await createAcesso(req.body);
+        const acessos = await Acessos.create(req.body);
         return res.status(200).send("Acesso adicionado com sucesso");
     } catch (err) {
-        console.error(err);
-        return res.status(400).send("Erro ao adicionar acesso");
+        return res.status(400).send("Erro ao adicionar acesso", res);
     }
 });
 
-app.get("/listarusuario/:id", async (req, res) => {
-    try {
-        const usuario = await getUsuarioById(req.params.id);
+
+app.get("/listarusuario/:id", (req, res) => {
+    Usuarios.findOne({ _id: req.params.id }).then((usuario) => {
         return res.json(usuario);
-    } catch (err) {
-        console.error(err);
+    }).catch((erro) => {
         return res.status(400).json({
             error: true,
             message: "Nenhum usuario com o id que você inseriu foi encontrado!"
         });
-    }
+    });
 });
 
-app.put("/usuarios/editar/:id", async (req, res) => {
-    try {
-        const usuario = await updateUsuario(req.params.id, req.body);
-        return res.status(200).json({
-            erro: false,
-            message: "Alterações feitas"
+app.put("/usuarios/editar/:id", (req, res) => {
+    const usuario = Usuarios.updateOne({_id: req.params.id}, req.body)
+        .exec()
+        .then(() => {
+            return res.status(200).json({
+                erro: false,
+                message: "Alterações feitas"
+            });
+        })
+        .catch((erro) => {
+            return res.status(400).json({
+                erro: true,
+                message: "Ocorreu um problema, tente novamente"
+            });
         });
-    } catch (err) {
-        console.error(err);
-        return res.status(400).json({
-            erro: true,
-            message: "Ocorreu um problema, tente novamente"
-        });
-    }
 });
 
-
-app.listen(port, () => {
+app.listen(port, ()=>{
     console.log(`Servidor iniciado no endereço: http://localhost:${port}`);
 });
