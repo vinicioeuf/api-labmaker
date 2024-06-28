af91bf1e17f8c10c5f607f61ce40d9af05b9876c
Vinicio Eufrazio <59233562+vinicioeuf@users.noreply.github.com>
2024-06-27 22:44:50 -0300

    .

--- models/Acessos.js
+++ models/Acessos.js
@@ -1,14 +1,18 @@
-// models/Acessos.js
 const pool = require('../e');
 
 const getAcessos = async () => {
+    console.log("Buscando todos os acessos");
     const res = await pool.query('SELECT * FROM acessos');
     return res.rows;
 };
 
 const createAcesso = async (data) => {
-    const { usuario_id, data_acesso } = data;
-    const res = await pool.query('INSERT INTO acessos (usuario_id, data_acesso) VALUES ($1, $2) RETURNING *', [usuario_id, data_acesso]);
+    console.log("Criando acesso com dados:", data);
+    const { nome, email, foto, idBiometria, tipo } = data;
+    const res = await pool.query(
+        'INSERT INTO acessos (nome, email, foto, "idBiometria", tipo, "createdAt") VALUES ($1, $2, $3, $4, $5, NOW()) RETURNING *',
+        [nome, email, foto, idBiometria, tipo]
+    );
     return res.rows[0];
 };
 
