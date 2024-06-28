ebd7d023e6b9a5450485e0e52dae3ba34abbd85f
Vinicio Eufrazio <59233562+vinicioeuf@users.noreply.github.com>
2024-06-27 23:08:11 -0300

    Revert "."
    This reverts commit af91bf1e17f8c10c5f607f61ce40d9af05b9876c.

--- models/Acessos.js
+++ models/Acessos.js
@@ -1,18 +1,14 @@
+// models/Acessos.js
 const pool = require('../e');
 
 const getAcessos = async () => {
-    console.log("Buscando todos os acessos");
     const res = await pool.query('SELECT * FROM acessos');
     return res.rows;
 };
 
 const createAcesso = async (data) => {
-    console.log("Criando acesso com dados:", data);
-    const { nome, email, foto, idBiometria, tipo } = data;
-    const res = await pool.query(
-        'INSERT INTO acessos (nome, email, foto, "idBiometria", tipo, "createdAt") VALUES ($1, $2, $3, $4, $5, NOW()) RETURNING *',
-        [nome, email, foto, idBiometria, tipo]
-    );
+    const { usuario_id, data_acesso } = data;
+    const res = await pool.query('INSERT INTO acessos (usuario_id, data_acesso) VALUES ($1, $2) RETURNING *', [usuario_id, data_acesso]);
     return res.rows[0];
 };
 
--- models/Usuarios.js
+++ models/Usuarios.js
@@ -1,29 +1,23 @@
+// models/Usuarios.js
 const pool = require('../e');
 
 const getUsuarios = async () => {
-    console.log("Buscando todos os usuários");
     const res = await pool.query('SELECT * FROM usuarios');
     return res.rows;
 };
 
 const getUsuarioById = async (id) => {
-    console.log(`Buscando usuário com id: ${id}`);
     const res = await pool.query('SELECT * FROM usuarios WHERE id = $1', [id]);
     return res.rows[0];
 };
 
 const createUsuario = async (data) => {
-    console.log("Criando usuário com dados:", data);
-    const { nome, email, idBiometria, foto, status } = data;
-    const res = await pool.query(
-        'INSERT INTO usuarios (nome, email, "idBiometria", foto, status, "createdAt") VALUES ($1, $2, $3, $4, $5, NOW()) RETURNING *',
-        [nome, email, idBiometria, foto, status]
-    );
+    const { nome, email } = data;
+    const res = await pool.query('INSERT INTO usuarios (nome, email) VALUES ($1, $2) RETURNING *', [nome, email]);
     return res.rows[0];
 };
 
 const updateUsuario = async (id, data) => {
-    console.log(`Atualizando usuário com id: ${id} com dados:`, data);
     const { nome, email } = data;
     const res = await pool.query('UPDATE usuarios SET nome = $1, email = $2 WHERE id = $3 RETURNING *', [nome, email, id]);
     return res.rows[0];
