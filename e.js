be28218be3f5c8f7fc38b29adb4cb288c5add8b3
Vinicio Eufrazio <59233562+vinicioeuf@users.noreply.github.com>
2025-03-23 19:26:30 -0300

    trocando banco

--- e.js
+++ e.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'u38qbi3ojpsa20',
-    host: 'ccba8a0vn4fb2p.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
-    database: 'd3fps7of3eqn7j',
-    password: 'pe8c0334852286e5c2189ec7991828147216f133cac7249c16864fdc50ddd930d',
+    user: 'postgres',
+    host: 'seemingly-frank-cisco.data-1.use1.tembo.io',
+    database: 'postgres',
+    password: 'ZUibujzg4IJlXQG7',
     port: 5432,
     ssl: {
         rejectUnauthorized: false
